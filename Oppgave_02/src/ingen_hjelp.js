3e1b2bf36732e54c3142640593f66cc7b1580b90
Julian Alsemmani <juliankonge@hotmail.com>
2022-09-05 11:08:03 +0200

    Update ingen_hjelp.js

--- Oppgave_02/src/ingen_hjelp.js
+++ Oppgave_02/src/ingen_hjelp.js
@@ -1,58 +1,58 @@
 const users = [{
-        id: 0,
-        name: "Trude",
-        age: 77
-    },
-    {
-        id: 1,
-        name: "Simen",
-        age: 85
-    },
-    {
-        id: 2,
-        name: "Lars",
-        age: 99
-    },
-    {
-        id: 3,
-        name: "Ali",
-        age: 34
-    },
-    {
-        id: 4,
-        name: "Finn",
-        age: 60
-    },
-    {
-        id: 5,
-        name: "Kåre",
-        age: 70
-    },
-    {
-        id: 6,
-        name: "Hanne",
-        age: 20
-    },
-    {
-        id: 7,
-        name: "Åse",
-        age: 21
-    },
-    {
-        id: 8,
-        name: "Anne",
-        age: 6
-    },
-    {
-        id: 9,
-        name: "Amanda",
-        age: 31
-    },
-    {
-        id: 10,
-        name: "Morgan",
-        age: 87
-    }
+    id: 0,
+    name: "Trude",
+    age: 77
+},
+{
+    id: 1,
+    name: "Simen",
+    age: 85
+},
+{
+    id: 2,
+    name: "Lars",
+    age: 99
+},
+{
+    id: 3,
+    name: "Ali",
+    age: 34
+},
+{
+    id: 4,
+    name: "Finn",
+    age: 60
+},
+{
+    id: 5,
+    name: "Kåre",
+    age: 70
+},
+{
+    id: 6,
+    name: "Hanne",
+    age: 20
+},
+{
+    id: 7,
+    name: "Åse",
+    age: 21
+},
+{
+    id: 8,
+    name: "Anne",
+    age: 6
+},
+{
+    id: 9,
+    name: "Amanda",
+    age: 31
+},
+{
+    id: 10,
+    name: "Morgan",
+    age: 87
+}
 ];
 
 // TODO: Hent HTML #id med getElementById
