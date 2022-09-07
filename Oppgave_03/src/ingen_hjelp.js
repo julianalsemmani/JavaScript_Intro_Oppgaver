4ca6bd499a88100fe6e5552fa7703f40e4fdd2c6
Julian Alsemmani <juliankonge@hotmail.com>
2022-09-07 14:07:26 +0200

    Update ingen_hjelp.js

--- Oppgave_03/src/ingen_hjelp.js
+++ Oppgave_03/src/ingen_hjelp.js
@@ -10,6 +10,15 @@ import './styles.css';
 // TODO: Lag en funksjon som gjør at vi kan bytte ord. Må også oppdatere tellerene
 // TODO: Lag en funksjon for å sjekke om vi har skrevet riktig bokstav. Må ta hensyn til plassen i ordet vi skal skrive
 // TODO: Lag en funksjon for å sjekke om posisjonen vi er på er lik lengden på ordet vi skal skrive. Det betyr at vi er ferdig med ordet og kan bytte ord
+<<<<<<< Updated upstream
+=======
+const checkPosition = () => {
+    if (position === word.length) {
+        changeWord();
+    }
+}
+
+>>>>>>> Stashed changes
 // TODO: Lag en funksjon som trigges når vi skriver på tastaturen og basert på hva vi skriver / posisjonen vi er på gjør nødvendige oppdateringer av grensesnittet
 // TODO: Lag en funksjon som kan brukes til å oppdatere grensesnittet
 // - Vise antall feil
